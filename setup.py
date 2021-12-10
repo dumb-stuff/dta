7b685007b39708af0687ccd5e354dd0248cbff91
Rukchad Wongprayoon <contact@biomooping.tk>
2021-12-10 12:51:41 +0700

    added py2 tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="dta",
-    version="1.1.1",
+    version="1.1.2",
     author="Rukchad Wongprayoon",
     author_email="contact@biomooping.tk",
     description="dta Convert Dict To Attributes!",
